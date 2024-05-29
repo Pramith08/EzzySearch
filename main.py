915f541b26d67b726834b11b81ff5e99dec8c339
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-05-29 23:16:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_no_of_results():
     while True:
